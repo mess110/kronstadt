f90faa33fdc8e3b6a14232bc7815e96c7d317667
Cristian Mircea Messel <mess110@gmail.com>
2012-03-12 15:40:45 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kronstadt::Application.config.secret_token = '8320b34a49259e165b1d6a5fd22470172548fdb0a19d3edc0f73c3c454497d7857a2b725d1017c33ffaffcbbbd263559f1982788ade4d70d349beb335886ce08'
