4a0e13ff125c739870770a5fc72a05320956f974
Cristian Mircea Messel <mess110@gmail.com>
2012-03-12 16:23:35 +0200

    include modules from lib folder

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Kronstadt
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += Dir["#{config.root}/lib", "#{config.root}/lib/**/"]
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
