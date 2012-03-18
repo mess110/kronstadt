485b963de7d50ac48c4fd19f69b33abc01086b5e
Cristian Mircea Messel <mess110@gmail.com>
2012-03-18 17:24:40 +0200

    git add if_config

---
+++ lib/if_config.rb
@@ -0,0 +1,10 @@
+class IfConfig
+  def self.ips
+    ips = []
+    inets = `/sbin/ifconfig | grep "inet addr"`.split(" ")
+    inets.each do |i|
+      ips << i.gsub("addr:","") if i =~ /\Aaddr:/
+    end
+    ips
+  end
+end
